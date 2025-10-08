c8d1127aa1c7097ff815f9f0f8865181019a9fd5
João Lourenço <jmguellourenco@gmail.com>
2025-10-08 16:07:28 +0100

    created xml object

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import requests
+from xml_object import XMLObject
+
 def main():
-    print("Hello from wsdl!")
+
+    url = "https://app.parlamento.pt/webutils/docs/doc.xml?path=piH4nM%2bH4zC3Ukcy9%2bb3N%2bmcH0oYAE56Je3uDxPEBW%2bO0reHX0bvAVrEGlMyPQkTfWZYvEZZ6SomUrhduWVcLI13ioaXb36QHHnSNPzWorVAsfHTALh8dkXCsUhypC7ZXSHPneBWqrLU7Sm4G1GbgAUmdG9N%2fLqFyiQZ46J%2fmstGcS%2fwoP85b31Ivqya3JW7Dgc7X7yS2OCDOAFHgDO88qaqM1SbUl1pcr2UzcS%2fPtRrICm4vJwQ3VUcAgTUm7zVB8P007q1ZcZ7nePXgsZDQyufM26tP8ySahuBNDrYSQR%2bC8jwPO7D04CG%2bqJZyDgX%2brqVQ0sSwCkR31NubN36TKVG3hu6RHwTy1%2fp74VGcZ9IwLNXa2gnztxzHL%2bkfEjTuaO9DIY%2bXvGtp2G1gX%2fYTV4x5tM%2brPSpfv3%2bv%2bcTQyQ%3d&fich=InformacaoBaseXVII.xml&Inline=true"
+
+    response = requests.get(url)
+    if response.status_code != 200:
+        SystemExit("Failed to fetch data")
+
+    # Remove BOM
+    cleaned_data = XMLObject.remove_bom(response.text)
+
+    xml_obj = XMLObject(XMLObject.toTreeElement(cleaned_data), cleaned_data)
+
+    print(xml_obj.get_string())
 
 
 if __name__ == "__main__":
---
+++ xml_object.py
@@ -0,0 +1,83 @@
+import logging
+from dataclasses import dataclass
+import xml.etree.ElementTree as ET
+
+logger = logging.getLogger(__name__)
+
+@dataclass
+class XMLObject:
+    """
+    Object representation of an XML object. Contains the element and string representations of the object.
+    Elements are represented using ElementTree.
+    """
+    element: ET.Element     # Represents the element representation of an XML object using ElementTree
+    string: str             # Represents the string representation of an XML object
+
+    @staticmethod
+    def print_element(element: ET.Element):
+        """
+        Print the element and its children.
+        """
+        logger.debug(ET.tostring(element).decode())
+
+    @staticmethod
+    def remove_bom(xml_string: str):
+        """
+        Remove the Byte Order Mark (BOM) from an XML string.
+        """
+        return xml_string[3:]
+    
+    @staticmethod
+    def toTreeElement(xml_string: str):
+        """
+        Parse an XML string and return the root element of the tree.
+        """
+        return ET.fromstring(xml_string)
+
+    def get_string(self):
+        """
+        Get the string representation of the XML object.
+        """
+        return self.string.encode('latin1').decode('utf-8') if self.string else None
+    
+    def get_tree_element(self):
+        """
+        Get the tree element representation of the XML object.
+        """
+        return self.element
+    
+    def find_first_element_by_name(self, element_name: str):
+        """
+        Find the first element with the given name in an XML string.
+        If Parsing Error occurs, print the error and the first 100 characters of the XML.
+
+        :param xml_string: XML string to search in.
+        :param element_name: Name of the element to find.
+
+        :return: XMLObject representing the first element with the given name.
+        """
+        try:
+            element = self.get_tree_element().find(f".//{element_name}")
+            return XMLObject(element, element.text) if element is not None else None
+        except ET.ParseError as e:
+            logger.error(f"XML Parsing Error: {e}")
+            logger.error(f"First 100 characters of XML: {repr(self.get_tree_element()[:100])}")
+            raise
+    
+    def find_elements_by_name(self , element_name: str):
+        """
+        Find all elements with a given name in an XML string.
+        If Parsing Error occurs, print the error and the first 100 characters of the XML.
+
+        :param xml_string: XML string to search in.
+        :param element_name: Name of the elements to find.
+
+        :return: List of XMLObjects representing the elements with the given name.
+        """
+        try:
+            element = self.get_tree_element().findall(f".//{element_name}")
+            return [XMLObject(e, e.text) for e in element]
+        except ET.ParseError as e:
+            logger.error(f"XML Parsing Error: {e}")
+            logger.error(f"First 100 characters of XML: {repr(self.get_tree_element()[:100])}")
+            raise
