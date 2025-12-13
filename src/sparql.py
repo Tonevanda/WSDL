f24c958322f8f4d49f074eee54d3996102b4a211
Tiago Cruz <tiagocruz057@gmail.com>
2025-12-13 11:47:11 +0000

    academic titles

--- src/sparql.py
+++ src/sparql.py
@@ -27,11 +27,33 @@ def query_sociologia_party(g: Graph):
     """
 
     for row in g.query(query):
-        print(f"{row.party} - {row.total} deputados Sociólogos")
+        print(f"{row.party} tem {row.total} deputados Sociólogos")
     
+def query_academic_titles_leg(g: Graph):
+     # Qual a quantidade de academic titles por legislatura
+    query = """
+    PREFIX : <http://www.semanticweb.org/tiago/ontologies/2025/11/poliontology/>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 
+    SELECT ?legDesc (COUNT(?title) as ?total)
+    WHERE {
+        ?mop a :MoP ;
+            :hasAcademicTitle ?title ;
+            :servedDuring ?ctx .
+        
+        ?ctx :legislature ?leg .
+        ?leg rdfs:label ?legDesc .
+        FILTER(LANG(?legDesc) = 'pt')
+    }
+    GROUP BY ?leg
+    ORDER BY DESC(?total)
+    """
+    
+    for row in g.query(query):
+        print(f"{row.legDesc} tem {row.total} títulos académicos")
 
-def queries(g: Graph):
+def query(g: Graph):
     
     query_area = """
     PREFIX : <http://www.semanticweb.org/tiago/ontologies/2025/11/poliontology/>
@@ -42,49 +64,17 @@ def queries(g: Graph):
     PREFIX wd: <http://www.wikidata.org/entity/>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
-    SELECT DISTINCT ?mopName ?circleName ?label
-    WHERE {
-        ?mop a :MoP ;
-            schema:name ?mopName ;
-            :servedDuring ?context .
-
-        ?context :electoralCircle ?circle ;
-            :membership ?membership ;
-            :situation ?situation ;
-            :legislature ?leg .
-
-        ?leg rdfs:label ?label .
-        FILTER(LANG(?label) = "pt")
-
-        ?membership :group :PCP .
-
-        ?situation :situationType ?sitType ;
-                   schema:startDate ?startDate .
-        
-        ?sitType skos:prefLabel ?situationType .
-        FILTER(LANG(?situationType) = "pt")
-        
-        ?circle rdfs:label ?circleName .
-        FILTER(LANG(?circleName) = "pt")
-
-        {
-            SELECT ?context (MAX(?date) AS ?maxDate)
-            WHERE {
-                ?context :situation ?s .
-                ?s schema:startDate ?date .
-            }
-            GROUP BY ?context
-        }
-        FILTER(?startDate = ?maxDate && ?situationType = "Efetivo")
-            
-        
-    }
-    ORDER BY ?mopName
     """
    
     for row in g.query(query_area):
         print(f"{row.mopName} - {row.circleName} - {row.label}")
 
-g = Graph()
-g.parse("./resources/test.ttl", format="turtle")
-query_sociologia_party(g)
+def main():
+    g = Graph()
+    g.parse("./resources/test.ttl", format="turtle")
+    query_academic_titles_leg(g)
+
+if __name__ == "__main__":
+    main()
+
+
